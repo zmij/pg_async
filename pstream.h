c5579b160aaa08127c18f8e3251ee11126fb291d
Jonathan Wakely <pstreams@kayari.org>
2006-01-21 21:02:06 +0000

    (pstreambuf::showmanyc()): Adjust result for sizeof(char_type) > 1

--- pstream.h
+++ pstream.h
@@ -1,4 +1,4 @@
-/* $Id: pstream.h,v 1.95 2005/09/05 01:15:27 redi Exp $
+/* $Id: pstream.h,v 1.96 2006/01/21 21:02:06 redi Exp $
 PStreams - POSIX Process I/O for C++
 Copyright (C) 2001,2002,2003,2004,2005 Jonathan Wakely
 
@@ -1678,9 +1678,9 @@ namespace redi
       int avail = 0;
 #ifdef FIONREAD
       if (ioctl(rpipe(), FIONREAD, &avail) == -1)
-        avail = -1;
+        avail = -sizeof(C);
 #endif
-      return std::streamsize(avail);
+      return std::streamsize(avail/sizeof(C));
     }
 
   /**
