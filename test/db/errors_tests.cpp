671b3ccb9f4cf5224419ff6ebdb716ebb3c61bde
zmij <sergei.a.fedorov@gmail.com>
2015-08-06 18:13:05 +0300

    Add failing query test

--- test/db/errors_tests.cpp
+++ test/db/errors_tests.cpp
@@ -277,3 +277,75 @@ TEST(ErrorTest, BreakQueryQueue)
 	}
 }
 
+TEST(ErrorTest, FailQueryQueue)
+{
+	using namespace tip::db::pg;
+	if (!test::environment::test_database.empty()) {
+
+		ASIO_NAMESPACE::deadline_timer timer(db_service::io_service(),
+				boost::posix_time::seconds(test::environment::deadline));
+		timer.async_wait([&](asio_config::error_code const& ec){
+			if (!ec) {
+				#ifdef WITH_TIP_LOG
+				local_log(logger::WARNING) << "Run query test timer expired";
+				#endif
+				db_service::stop();
+			}
+		});
+
+		resultset res;
+
+		std::vector<bool> query_resultsets(3, false);
+		int transaction_error = 0;
+		int query_error = 0;
+
+		ASSERT_NO_THROW(db_service::add_connection(test::environment::test_database,
+				test::environment::connection_pool));
+		connection_options opts = connection_options::parse(test::environment::test_database);
+		{
+			db_service::begin(opts.alias,
+			[&]( transaction_ptr tran ){
+				// Syntax error
+				query (tran, "create temporary table pg_async_test(bigint)")(
+				[&](transaction_ptr c, resultset, bool){
+					local_log(logger::DEBUG) << "Query one finished";
+					EXPECT_TRUE(c.get());
+					query_resultsets[0] = true;
+				}, [&](error::db_error const&){
+					query_error++;
+				});
+				query(tran, "select * from pg_async_test")
+				([&](transaction_ptr c, resultset r, bool) {
+					local_log(logger::DEBUG) << "Query two finished";
+					EXPECT_TRUE(c.get());
+					query_resultsets[1] = true;
+				}, [&](error::db_error const&){
+					query_error++;
+				});
+				query(tran, "drop table pg_async_test")
+				([&](transaction_ptr c, resultset r, bool) {
+					local_log(logger::DEBUG) << "Query three finished";
+					EXPECT_TRUE(c.get());
+					query_resultsets[2] = true;
+
+				}, [&](error::db_error const&){
+					query_error++;
+				});
+				tran->commit();
+			}, [&](error::db_error const& e){
+				local_log(logger::DEBUG) << "Transaction error callback fired: "
+						<< e.what();
+				transaction_error++;
+                timer.cancel();
+				db_service::stop();
+			});
+		}
+		db_service::run();
+		EXPECT_FALSE(query_resultsets[0]);
+		EXPECT_FALSE(query_resultsets[1]);
+		EXPECT_FALSE(query_resultsets[2]);
+		EXPECT_EQ(1, query_error);
+		EXPECT_EQ(1, transaction_error);
+	}
+}
+
