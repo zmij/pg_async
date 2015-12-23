44f261cdd17416fd6b38ff536e6aeb7b90df4551
melnikov <al.melnikov@corp.mail.ru>
2015-12-23 15:26:40 +0300

    Test lib.class

---
+++ Source/Awm/Private/UI/AwmWidgetFunctionLibrary.cpp
@@ -0,0 +1,7 @@
+// Copyright 2015 Mail.Ru Group. All Rights Reserved.
+
+#include "Awm.h"
+
+void UAwmWidgetFunctionLibrary::GetWidgetAbsolutePosition(UUserWidget* Widget, FVector2D& AbsolutePosition) {
+
+}
