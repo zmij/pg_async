62402aacd7bfceccc1bf6980ba6675de2dfd6ae5
zmij <sergei.a.fedorov@gmail.com>
2015-12-28 18:46:52 +0300

    Generate correct alias for pg asyn tests

--- Server/sql/create/set_db_options.cmake
+++ Server/sql/create/set_db_options.cmake
@@ -3,9 +3,9 @@ build_db_options(AUTHN)
 build_db_options(LOBBY)
 
 if(DB_AUTHN_DSN)
-    set(_TEST_DATABASE ${DB_AUTHN_DSN})
+    set(_TEST_DATABASE "main=${DB_AUTHN_DSN}")
 elseif(DB_LOBBY_DSN)
-    set(_TEST_DATABASE ${DB_LOBBY_DSN})
+    set(_TEST_DATABASE "main=${DB_LOBBY_DSN}")
 endif()
 
 if(_TEST_DATABASE)
