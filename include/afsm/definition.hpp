3e9599efc35b66d0067a1dbca4178cbd1cd4ae80
zmij <sergei.a.fedorov@gmail.com>
2016-06-01 17:46:15 +0300

    Squashed 'lib/afsm/' content from commit b1ce337
    git-subtree-dir: lib/afsm
    git-subtree-split: b1ce33776a9ca19c5ef158e819c1dcc681785514

---
+++ include/afsm/definition.hpp
@@ -0,0 +1,463 @@
+/*
+ * definition.hpp
+ *
+ *  Created on: 27 мая 2016 г.
+ *      Author: sergey.fedorov
+ */
+
+#ifndef AFSM_DEFINITION_HPP_
+#define AFSM_DEFINITION_HPP_
+
+#include <pushkin/meta.hpp>
+#include <afsm/fsm_fwd.hpp>
+
+namespace afsm {
+
+namespace def {
+template < typename SourceState, typename Event, typename TargetState,
+        typename Action = none, typename Guard = none >
+struct transition;
+template < typename Event, typename Action = none, typename Guard = none >
+struct internal_transition;
+template < typename ... T >
+struct transition_table;
+template < typename T, bool History = false, typename CommonBase = void >
+struct state;
+template < typename T, typename CommonBase = void  >
+struct terminal_state;
+
+template < typename T, bool History = false, typename CommonBase = void >
+struct state_machine;
+
+namespace detail {
+
+template <typename T>
+struct source_state;
+
+template < typename SourceState, typename Event, typename TargetState,
+    typename Action, typename Guard>
+struct source_state< transition<SourceState, Event, TargetState, Action, Guard> > {
+    using type = SourceState;
+};
+
+template < typename Event, typename Action, typename Guard >
+struct source_state< internal_transition< Event, Action, Guard > > {
+    using type = void;
+};
+
+template <typename T>
+struct target_state;
+
+template < typename SourceState, typename Event, typename TargetState,
+    typename Action, typename Guard>
+struct target_state< transition<SourceState, Event, TargetState, Action, Guard> > {
+    using type = TargetState;
+};
+
+template < typename Event, typename Action, typename Guard >
+struct target_state< internal_transition< Event, Action, Guard > > {
+    using type = void;
+};
+
+template <typename T>
+struct event_type;
+
+template < typename SourceState, typename Event, typename TargetState,
+    typename Action, typename Guard>
+struct event_type< transition<SourceState, Event, TargetState, Action, Guard> > {
+    using type = Event;
+};
+template < typename Event, typename Action, typename Guard >
+struct event_type< internal_transition< Event, Action, Guard > > {
+    using type = Event;
+};
+
+
+template < typename T >
+struct is_transition : ::std::false_type {};
+
+template < typename SourceState, typename Event, typename TargetState,
+    typename Action, typename Guard>
+struct is_transition< transition<SourceState, Event, TargetState, Action, Guard> >
+    : ::std::true_type{};
+template < typename Event, typename Action, typename Guard >
+struct is_transition< internal_transition< Event, Action, Guard > >
+    : ::std::true_type {};
+
+template < typename T >
+struct is_internal_transition : ::std::false_type {};
+
+template < typename Event, typename Action, typename Guard >
+struct is_internal_transition< internal_transition< Event, Action, Guard > >
+    : ::std::true_type {};
+
+template < typename T >
+struct is_state : ::std::conditional<
+        ::std::is_base_of< state<T, true>, T >::value
+         || ::std::is_base_of< state<T, false>, T >::value
+         || ::std::is_base_of< terminal_state<T>, T >::value,
+        ::std::true_type,
+        ::std::false_type
+    >::type {};
+
+template < typename T >
+struct is_terminal_state : ::std::conditional<
+        ::std::is_base_of< terminal_state<T>, T >::value,
+        ::std::true_type,
+        ::std::false_type
+    >::type {};
+
+template <typename T>
+struct state_machine_trait {};
+
+template < typename T >
+struct is_state_machine : ::std::conditional<
+        ::std::is_base_of< state_machine_trait<T>, T >::value,
+        ::std::true_type,
+        ::std::false_type
+    >::type {};
+
+template <typename T>
+struct common_base_trait {};
+
+template < typename T >
+struct has_common_base : ::std::conditional<
+        ::std::is_base_of< common_base_trait<T>, T >::value,
+        ::std::true_type,
+        ::std::false_type
+    >::type {};
+
+template < typename SourceState, typename Event, typename Guard >
+struct transition_key {
+    using source_state_type     = SourceState;
+    using event_type            = Event;
+    using guard_type            = Guard;
+};
+
+}  /* namespace detail */
+
+template < typename SourceState, typename Event, typename TargetState,
+        typename Action, typename Guard >
+struct transition {
+    using source_state_type     = SourceState;
+    using target_state_type     = TargetState;
+    using event_type            = Event;
+    using action_type           = Action;
+    using guard_type            = Guard;
+
+    using key_type  = detail::transition_key<SourceState, Event, Guard>;
+
+    struct value_type    {
+        using action_type       = transition::action_type;
+        using target_state_type = transition::target_state_type;
+    };
+};
+
+template < typename Event, typename Action, typename Guard >
+struct internal_transition {
+    using event_type            = Event;
+    using action_type           = Action;
+    using guard_type            = Guard;
+
+    using key_type  = detail::transition_key<void, Event, Guard>;
+
+    struct value_type    {
+        using action_type       = internal_transition::action_type;
+    };
+
+    static_assert(!::std::is_same<Event, none>::value,
+            "Internal transition must have a trigger");
+};
+
+template < typename Event >
+struct handles_event {
+    template < typename Transition >
+    struct type : ::std::conditional<
+        ::std::is_same< typename Transition::event_type, Event >::value,
+        ::std::true_type,
+        ::std::false_type
+    >::type {};
+};
+
+template < typename State >
+struct originates_from {
+    template < typename Transition >
+    struct type : ::std::conditional<
+        ::std::is_same< typename Transition::source_state_type, State >::value,
+        ::std::true_type,
+        ::std::false_type
+    >::type {};
+};
+
+template < typename ... T >
+struct transition_table {
+    static_assert(
+            ::std::conditional<
+                (sizeof ... (T) > 0),
+                ::psst::meta::all_match< detail::is_transition, T ... >,
+                ::std::true_type
+            >::type::value,
+            "Transition table can contain only transition or internal_transition template instantiations" );
+    using transitions = ::psst::meta::type_tuple<T...>;
+    using transition_map = ::psst::meta::type_map<
+            ::psst::meta::type_tuple<typename T::key_type ...>,
+            ::psst::meta::type_tuple<typename T::value_type...>>;
+    using inner_states  = typename ::psst::meta::unique<
+                typename detail::source_state<T>::type ...,
+                typename detail::target_state<T>::type ...
+            >::type;
+    using handled_events = typename ::psst::meta::unique<
+            typename T::event_type ... >::type;
+
+    static constexpr ::std::size_t size                 = transition_map::size;
+    static constexpr ::std::size_t transition_count     = transition_map::size;
+    static constexpr ::std::size_t inner_state_count    = inner_states::size;
+    static constexpr ::std::size_t event_count          = handled_events::size;
+
+    static_assert(
+            ::std::conditional<
+                 (inner_state_count > 0),
+                 ::psst::meta::all_match< detail::is_state, inner_states >,
+                 ::std::true_type
+            >::type::value,
+            "State types must derive from afsm::def::state");
+    static_assert(transitions::size == transition_count, "Duplicate transition");
+    // TODO Check for different guards for transitions from one state on one event
+};
+
+template < typename StateType, bool HasHistory >
+struct state< StateType, HasHistory, void > {
+    static constexpr bool has_history = HasHistory;
+
+    using state_type            = StateType;
+    using base_state_type       = state<state_type, has_history, void>;
+    using internal_transitions  = void;
+    using transitions           = void;
+    using deferred_events       = void;
+    using activity              = void;
+
+    template < typename Event, typename Action = none, typename Guard = none >
+    using in = internal_transition< Event, Action, Guard>;
+    template < typename ... T >
+    using transition_table = def::transition_table<T...>;
+
+    using none = afsm::none;
+    template < typename Predicate >
+    using not_ = ::psst::meta::not_<Predicate>;
+    template < typename ... T >
+    using type_tuple = ::psst::meta::type_tuple<T...>;
+};
+
+template < typename StateType, bool HasHistory, typename CommonBase >
+struct state : state<StateType, HasHistory, void>, CommonBase,
+        detail::common_base_trait<StateType> {
+    static constexpr bool has_history = HasHistory;
+    using state_type            = StateType;
+    using base_state_type       = state<state_type, has_history, CommonBase>;
+    using internal_transitions  = void;
+    using transitions           = void;
+    using deferred_events       = void;
+    using activity              = void;
+    using common_base           = CommonBase;
+};
+
+template < typename StateType >
+struct terminal_state< StateType, void > {
+    using state_type            = StateType;
+    using base_state_type       = terminal_state<state_type, void>;
+    using internal_transitions  = void;
+    using transitions           = void;
+    using deferred_events       = void;
+    using activity              = void;
+};
+
+template < typename StateType, typename CommonBase >
+struct terminal_state : terminal_state<StateType, void>, CommonBase {
+    using state_type            = StateType;
+    using base_state_type       = terminal_state<state_type, void>;
+};
+
+template < typename StateMachine, bool HasHistory, typename CommonBase >
+struct state_machine : state< StateMachine, HasHistory, CommonBase >,
+        detail::state_machine_trait<StateMachine> {
+    static constexpr bool has_history = HasHistory;
+    using state_machine_type    = StateMachine;
+    using base_state_type       = state_machine< state_machine_type, has_history, CommonBase >;
+    using initial_state         = void;
+    using internal_transitions  = void;
+    using transitions           = void;
+    using deferred_events       = void;
+    using activity              = void;
+
+    template <typename SourceState, typename Event, typename TargetState,
+            typename Action = none, typename Guard = none>
+    using tr = transition<SourceState, Event, TargetState, Action, Guard>;
+
+    template < typename T, bool History = false, typename Base = CommonBase >
+    using state = def::state<T, History, Base>;
+
+    template < typename T, typename Base = CommonBase >
+    using terminal_state = def::terminal_state<T, Base>;
+
+    using none = afsm::none;
+    template < typename Predicate >
+    using not_ = ::psst::meta::not_<Predicate>;
+    template < typename ... T >
+    using type_tuple = ::psst::meta::type_tuple<T...>;
+};
+
+namespace detail {
+
+template < typename T >
+struct has_inner_states : ::std::false_type {};
+template < typename ... T >
+struct has_inner_states< transition_table<T...> >
+    : ::std::integral_constant<bool,
+        (transition_table<T...>::inner_state_count > 0)> {};
+
+template < typename T >
+struct inner_states {
+    using type = ::psst::meta::type_tuple<>;
+};
+
+template < typename ... T >
+struct inner_states< transition_table<T...> > {
+    using type = typename transition_table<T...>::inner_states;
+};
+
+template < typename T >
+struct has_transitions : ::std::false_type {};
+template < typename ... T >
+struct has_transitions< transition_table<T...> >
+    : ::std::conditional<
+        (transition_table<T...>::transition_count > 0),
+        ::std::true_type,
+        ::std::false_type
+    >::type{};
+
+template < typename T >
+struct handled_events
+    : std::conditional<
+          is_state_machine<T>::value,
+          handled_events<state_machine<T>>,
+          handled_events<state<T>>
+      >::type {};
+
+template < typename ... T >
+struct handled_events< transition_table<T...> > {
+    using type = typename transition_table<T...>::handled_events;
+};
+
+template <>
+struct handled_events<void> {
+    using type = ::psst::meta::type_tuple<>;
+};
+
+template < typename T >
+struct handled_events< state<T> > {
+    using type = typename handled_events<
+            typename T::internal_transitions >::type;
+};
+
+template < typename T >
+struct handled_events< state_machine<T> > {
+    using type = typename ::psst::meta::unique<
+                typename handled_events< typename T::internal_transitions >::type,
+                typename handled_events< typename T::transitions >::type
+            >::type;
+};
+
+/**
+ * Events handled by a set of states
+ */
+template < typename ... T >
+struct handled_events< ::psst::meta::type_tuple<T...> > {
+    using type = typename ::psst::meta::unique<
+                typename handled_events<T>::type ...
+            >::type;
+};
+
+template < typename T >
+struct recursive_handled_events
+    : ::std::conditional<
+        is_state_machine<T>::value,
+        recursive_handled_events< state_machine<T> >,
+        handled_events< state<T> >
+    >::type {};
+
+template < typename ... T >
+struct recursive_handled_events< transition_table<T...> > {
+    using type = typename ::psst::meta::unique<
+            typename transition_table<T...>::handled_events,
+            typename recursive_handled_events<
+                typename transition_table<T...>::inner_states >::type
+        >::type;
+};
+
+template <>
+struct recursive_handled_events<void> {
+    using type = ::psst::meta::type_tuple<>;
+};
+
+template < typename T >
+struct recursive_handled_events< state_machine<T> > {
+    using type = typename ::psst::meta::unique<
+                typename handled_events< typename T::internal_transitions >::type,
+                typename recursive_handled_events< typename T::transitions >::type
+            >::type;
+};
+
+template < typename T, typename ... Y >
+struct recursive_handled_events< ::psst::meta::type_tuple<T, Y...> > {
+    using type = typename ::psst::meta::unique<
+                typename recursive_handled_events<T>::type,
+                typename recursive_handled_events< ::psst::meta::type_tuple<Y...>>::type
+            >::type;
+};
+
+template < typename T >
+struct recursive_handled_events< ::psst::meta::type_tuple<T> >
+    : recursive_handled_events<T> {};
+
+template <>
+struct recursive_handled_events< ::psst::meta::type_tuple<> > {
+    using type = ::psst::meta::type_tuple<>;
+};
+
+template < typename T >
+struct has_default_transitions;
+
+template < typename ... T >
+struct has_default_transitions< ::psst::meta::type_tuple<T...> >
+    : ::psst::meta::contains< none, ::psst::meta::type_tuple<T...> > {};
+
+template < typename T >
+struct is_default_transition
+    : ::std::conditional<
+        ::std::is_same< typename T::event_type, none >::value,
+        ::std::true_type,
+        ::std::false_type
+    >::type {};
+
+template < typename T >
+struct is_unguarded_transition
+    : ::std::conditional<
+        ::std::is_same< typename T::guard_type, none >::value,
+        ::std::true_type,
+        ::std::false_type
+    >::type {};
+
+template < typename T >
+struct is_default_unguarded_transition
+    : ::std::conditional<
+        is_default_transition<T>::value && is_unguarded_transition<T>::value,
+        ::std::true_type,
+        ::std::false_type
+    >::type {};
+
+}  /* namespace detail */
+}  /* namespace def */
+}  /* namespace afsm */
+
+
+#endif /* AFSM_DEFINITION_HPP_ */
---
+++ include/afsm/fsm.hpp
@@ -0,0 +1,269 @@
+/*
+ * fsm.hpp
+ *
+ *  Created on: 25 мая 2016 г.
+ *      Author: sergey.fedorov
+ */
+
+#ifndef AFSM_FSM_HPP_
+#define AFSM_FSM_HPP_
+
+#include <afsm/detail/base_states.hpp>
+#include <deque>
+
+namespace afsm {
+
+template < typename FSM, typename T >
+class state : public detail::state_base< T > {
+public:
+    using enclosing_fsm_type    = FSM;
+public:
+    state(enclosing_fsm_type& fsm)
+        : state::state_type{}, fsm_{fsm}
+    {}
+    state(enclosing_fsm_type& fsm, state const& rhs)
+        : state::state_type{rhs}, fsm_{fsm}
+    {}
+
+    template < typename Event >
+    actions::event_process_result
+    process_event( Event&& evt )
+    {
+        return process_event_impl(::std::forward<Event>(evt),
+                detail::event_process_selector<
+                    Event,
+                    typename state::handled_events,
+                    typename state::deferred_events>{} );
+    }
+
+    enclosing_fsm_type&
+    enclosing_fsm()
+    { return fsm_; }
+    enclosing_fsm_type const&
+    enclosing_fsm() const
+    { return fsm_; }
+private:
+    template < typename Event >
+    actions::event_process_result
+    process_event_impl(Event&& evt,
+        detail::process_type<actions::event_process_result::process> const&)
+    {
+        return actions::handle_in_state_event(::std::forward<Event>(evt), fsm_, *this);
+    }
+    template < typename Event >
+    actions::event_process_result
+    process_event_impl(Event&&,
+        detail::process_type<actions::event_process_result::defer> const&)
+    {
+        return actions::event_process_result::defer;
+    }
+    template < typename Event >
+    actions::event_process_result
+    process_event_impl(Event&&,
+        detail::process_type<actions::event_process_result::refuse> const&)
+    {
+        return actions::event_process_result::refuse;
+    }
+private:
+    enclosing_fsm_type&    fsm_;
+};
+
+template < typename FSM, typename T >
+class inner_state_machine : public detail::state_machine_base< T, none > {
+public:
+    using enclosing_fsm_type    = FSM;
+    using base_machine_type     = detail::state_machine_base< T, none >;
+public:
+    inner_state_machine(enclosing_fsm_type& fsm)
+        : base_machine_type{}, fsm_{fsm} {}
+    inner_state_machine(enclosing_fsm_type& fsm, inner_state_machine const& rhs)
+        : base_machine_type{rhs}, fsm_{fsm} {}
+
+    template < typename Event >
+    actions::event_process_result
+    process_event( Event&& event )
+    {
+        return process_event_impl(fsm_, ::std::forward<Event>(event),
+                detail::event_process_selector<
+                    Event,
+                    typename inner_state_machine::handled_events,
+                    typename inner_state_machine::deferred_events>{} );
+    }
+
+    enclosing_fsm_type&
+    enclosing_fsm()
+    { return fsm_; }
+    enclosing_fsm_type const&
+    enclosing_fsm() const
+    { return fsm_; }
+private:
+    using base_machine_type::process_event_impl;
+private:
+    enclosing_fsm_type&    fsm_;
+};
+
+template < typename T, typename Mutex >
+class state_machine : public detail::state_machine_base< T, Mutex > {
+public:
+    static_assert( ::psst::meta::is_empty< typename T::deferred_events >::value,
+            "Outer state machine cannot defer events" );
+    using this_type         = state_machine<T, Mutex>;
+    using base_machine_type = detail::state_machine_base< T, Mutex >;
+public:
+    state_machine()
+        : base_machine_type{},
+          stack_size_{0},
+          mutex_{},
+          queued_events_{},
+          deferred_mutex_{},
+          deferred_events_{}
+      {}
+    template<typename ... Args>
+    explicit
+    state_machine(Args&& ... args)
+        : base_machine_type(::std::forward<Args>(args)...),
+          stack_size_{0},
+          mutex_{},
+          queued_events_{},
+          deferred_mutex_{},
+          deferred_events_{}
+    {}
+
+    template < typename Event >
+    actions::event_process_result
+    process_event( Event&& event )
+    {
+        if (!stack_size_++) {
+            auto res = process_event_dispatch(::std::forward<Event>(event));
+            lock_guard lock{mutex_};
+            // Process enqueued events
+            --stack_size_;
+            return res;
+        } else {
+            --stack_size_;
+            // Enqueue event
+            enqueue_event(::std::forward<Event>(event));
+            return actions::event_process_result::defer;
+        }
+    }
+private:
+    template < typename Event >
+    actions::event_process_result
+    process_event_dispatch( Event&& event )
+    {
+        return process_event_impl(::std::forward<Event>(event),
+                    detail::event_process_selector<
+                        Event,
+                        typename state_machine::handled_events>{} );
+    }
+
+    template < typename Event >
+    actions::event_process_result
+    process_event_impl(Event&& event,
+        detail::process_type<actions::event_process_result::process> const& sel)
+    {
+        using actions::event_process_result;
+        auto res = base_machine_type::process_event_impl(*this, ::std::forward<Event>(event), sel );
+        switch (res) {
+            case event_process_result::process:
+                // Changed state. Process deferred events
+                process_deferred_queue();
+                break;
+            case event_process_result::defer:
+                // Add event to deferred queue
+                defer_event(::std::forward<Event>(event));
+                break;
+            case event_process_result::refuse:
+                // The event cannot be processed in current state
+                break;
+            default:
+                break;
+        }
+        return res;
+    }
+    template < typename Event >
+    actions::event_process_result
+    process_event_impl(Event&&,
+        detail::process_type<actions::event_process_result::refuse> const&)
+    {
+        static_assert( detail::event_process_selector<
+                Event,
+                typename state_machine::handled_events,
+                typename state_machine::deferred_events>::value
+            != actions::event_process_result::refuse,
+            "Event type is not handled by this state machine" );
+        return actions::event_process_result::refuse;
+    }
+
+    template < typename Event >
+    void
+    enqueue_event(Event&& event)
+    {
+        lock_guard lock{mutex_};
+        Event evt = event;
+        queued_events_.emplace_back([&, evt]() mutable {
+            return process_event_dispatch(::std::move(evt));
+        });
+    }
+
+    void
+    process_event_queue()
+    {
+
+    }
+
+    template < typename Event >
+    void
+    defer_event(Event&& event)
+    {
+        lock_guard lock{deferred_mutex_};
+        Event evt = event;
+        deferred_events_.emplace_back([&, evt]() mutable {
+            return process_event_dispatch(::std::move(evt));
+        });
+    }
+    void
+    process_deferred_queue()
+    {
+        if (stack_size_++ <= 1) {
+
+            event_queue deferred;
+            {
+                lock_guard lock{deferred_mutex_};
+                ::std::swap(deferred_events_, deferred);
+            }
+            while (!deferred.empty()) {
+                bool next_state = false;
+                while (!next_state && !deferred.empty()) {
+                    auto event = deferred.front();
+                    deferred.pop_front();
+                    next_state = event() == actions::event_process_result::process;
+                }
+                if (next_state) {
+                    lock_guard lock{deferred_mutex_};
+                    deferred_events_.insert(deferred_events_.end(), deferred.begin(), deferred.end());
+                    ::std::swap(deferred_events_, deferred);
+                }
+            }
+        }
+        --stack_size_;
+    }
+private:
+    using mutex_type        = Mutex;
+    using lock_guard        = typename detail::lock_guard_type<mutex_type>::type;
+    using atomic_counter    = ::std::atomic< ::std::size_t >;
+    using event_invokation  = ::std::function< actions::event_process_result() >;
+    using event_queue       = ::std::deque< event_invokation >;
+
+    atomic_counter          stack_size_;
+
+    mutex_type              mutex_;
+    event_queue             queued_events_;
+
+    mutex_type              deferred_mutex_;
+    event_queue             deferred_events_;
+};
+
+}  /* namespace afsm */
+
+#endif /* AFSM_FSM_HPP_ */
