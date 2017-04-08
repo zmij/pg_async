9f085f6211e117539bf8df621754d33c928a65a0
zmij <sergei.a.fedorov@gmail.com>
2017-04-08 17:06:54 +0300

    Squashed 'lib/asio-fiber/' changes from c794466..07332c6
    07332c6 Split shared work algorithm into two classes.
    e91a1c2 Mark boost includes as system (to ignore warnings)
    
    git-subtree-dir: lib/asio-fiber
    git-subtree-split: 07332c6301b98d9c7e0df44cc372bded5aae6ada

--- include/pushkin/asio/fiber/shared_work.hpp
+++ include/pushkin/asio/fiber/shared_work.hpp
@@ -22,18 +22,33 @@ namespace psst {
 namespace asio {
 namespace fiber {
 
-class shared_work : public ::boost::fibers::algo::algorithm {
+/**
+ * Scheduling algorithm for fiber dispatcher for use with one
+ * ::boost::asio::io_service in multiple threads.
+ *
+ * Usage synopsis:
+ *
+ * In each thread that should be running io_service loop together with
+ * fiber dispatcher:
+ *
+ * @code
+ * auto runner = ::psst::asio::fiber::use_shared_work_algorithm( io_svc );
+ * runner->run();
+ * @endcode
+ *
+ */
+
+class runner {
 public:
     using time_point            = ::std::chrono::steady_clock::time_point;
 
     using context               = ::boost::fibers::context;
     using context_type          = ::boost::fibers::type;
     using ready_queue_t         = ::boost::fibers::scheduler::ready_queue_t;
+
     using mutex_type            = ::boost::fibers::mutex;
     using condition_variable    = ::boost::fibers::condition_variable;
-
     using lock_type             = ::std::unique_lock<mutex_type>;
-
 private:
     struct service : io_service::service {
         using mutex_type        = ::std::mutex;
@@ -65,25 +80,10 @@ private:
         }
     };
 
-    struct impl {
-        mutex_type                              mtx_{};
-        condition_variable                      cnd_{};
-        ready_queue_t                           local_queue_{};
-
-        service*                                shared_{nullptr};
-
-        bool
-        has_ready_fibers() const noexcept
-        {
-            service::lock_type lock{ shared_->mtx_ };
-            return !shared_->queue_.empty() || !local_queue_.empty();
-        }
-    };
 public:
-    shared_work(io_service_ptr io_svc)
+    runner(io_service_ptr io_svc)
         : io_svc_{io_svc},
-          suspend_timer_{*io_svc},
-          pimpl_{ ::std::make_shared<impl>() }
+          suspend_timer_{ *io_svc }
     {
         try {
             ::std::unique_ptr< service > svc{new service(*io_svc_)};
@@ -92,62 +92,133 @@ public:
         } catch (::asio_ns::service_already_exists const& e) {
             // Ignore it here
         }
-        pimpl_->shared_ = &::asio_ns::use_service< service >(*io_svc_);
-        auto pimpl = pimpl_;
-        // Post the lambda
-        io_svc_->post(
-        [io_svc, pimpl]() mutable
-        {
-            while (!io_svc->stopped()) {
-                if (pimpl->has_ready_fibers()) {
-                    while (io_svc->poll());
+        shared_ = &::asio_ns::use_service< service >(*io_svc_);
+    }
 
-                    lock_type lock{pimpl->mtx_};
-                    pimpl->cnd_.wait_for(lock, ::std::chrono::milliseconds{10});
-                } else {
-                    if (!io_svc->run_one()) {
-                        break;
-                    }
+    ~runner()
+    {
+    }
+
+    void
+    run()
+    {
+        while (!io_svc_->stopped()) {
+            if (has_ready_fibers()) {
+                while (io_svc_->poll());
+
+                lock_type lock{mtx_};
+                cnd_.wait(lock);
+            } else {
+                if (!io_svc_->run_one()) {
+                    break;
                 }
             }
-        });
+        }
     }
-    ~shared_work()
+
+    void
+    stop()
     {
-        //cnd_.notify_one();
+        io_svc_->stop();
+        cnd_.notify_one();
+    }
+
+    bool
+    has_ready_fibers() const noexcept
+    {
+        service::lock_type lock{ shared_->mtx_ };
+        return !shared_->queue_.empty() || !local_queue_.empty();
     }
 
     void
     awakened(context* ctx) noexcept
     {
         if (ctx->is_context( context_type::pinned_context )) {
-            ctx->ready_link(pimpl_->local_queue_);
+            ctx->ready_link(local_queue_);
         } else {
             ctx->detach();
-            service::lock_type lock{ pimpl_->shared_->mtx_ };
-            ctx->ready_link( pimpl_->shared_->queue_ );
+            service::lock_type lock{ shared_->mtx_ };
+            ctx->ready_link( shared_->queue_ );
         }
+    }
+    context*
+    pick_next() noexcept
+    {
+        context* ctx{ nullptr };
+        service::lock_type lock{ shared_->mtx_ };
+        if (!shared_->queue_.empty()) {
+            ctx = &shared_->queue_.front();
+            shared_->queue_.pop_front();
+            lock.unlock();
+            context::active()->attach(ctx);
+        } else {
+            lock.unlock();
+            if (!local_queue_.empty()) {
+                ctx = &local_queue_.front();
+                local_queue_.pop_front();
+            }
+        }
+
+        return ctx;
+    }
+
+    void
+    suspend_until(time_point const& abs_time) noexcept
+    {
+        if (abs_time != time_point::max()) {
+            suspend_timer_.expires_at(abs_time);
+            suspend_timer_.async_wait(
+                []( error_code const& )
+                {
+                    ::boost::this_fiber::yield();
+                });
+        }
+        cnd_.notify_one();
+    }
+
+    void
+    notify() noexcept
+    {
+        suspend_timer_.async_wait([]( error_code const& )
+                {
+                    ::boost::this_fiber::yield();
+                });
+        suspend_timer_.expires_at(::std::chrono::steady_clock::now());
+    }
+private:
+    io_service_ptr                          io_svc_;
+    ::asio_ns::steady_timer                 suspend_timer_;
+
+    mutex_type                              mtx_{};
+    condition_variable                      cnd_{};
+    ready_queue_t                           local_queue_{};
+
+    service*                                shared_{nullptr};
+};
+using runner_ptr = ::std::shared_ptr<runner>;
+
+
+class shared_work : public ::boost::fibers::algo::algorithm {
+public:
+    using time_point            = ::std::chrono::steady_clock::time_point;
+    using context               = ::boost::fibers::context;
+public:
+    shared_work(runner_ptr r)
+        : pimpl_{ r } {}
+    ~shared_work()
+    {
+    }
+
+    void
+    awakened(context* ctx) noexcept
+    {
+        pimpl_->awakened(ctx);
     }
 
     context*
     pick_next() noexcept
     {
-        context* ctx{ nullptr };
-        service::lock_type lock{ pimpl_->shared_->mtx_ };
-        if (!pimpl_->shared_->queue_.empty()) {
-            ctx = &pimpl_->shared_->queue_.front();
-            pimpl_->shared_->queue_.pop_front();
-            lock.unlock();
-            context::active()->attach(ctx);
-        } else {
-            lock.unlock();
-            if (!pimpl_->local_queue_.empty()) {
-                ctx = &pimpl_->local_queue_.front();
-                pimpl_->local_queue_.pop_front();
-            }
-        }
-
-        return ctx;
+        return pimpl_->pick_next();
     }
 
     bool
@@ -159,34 +230,29 @@ public:
     void
     suspend_until(time_point const& abs_time) noexcept override
     {
-        if (abs_time != time_point::max()) {
-            suspend_timer_.expires_at(abs_time);
-            suspend_timer_.async_wait(
-                []( error_code const& )
-                {
-                    ::boost::this_fiber::yield();
-                });
-        }
-        pimpl_->cnd_.notify_one();
+        pimpl_->suspend_until(abs_time);
     }
 
     void
     notify() noexcept override
     {
-        suspend_timer_.async_wait([]( error_code const& )
-                {
-                    ::boost::this_fiber::yield();
-                });
-        suspend_timer_.expires_at(::std::chrono::steady_clock::now());
+        pimpl_->notify();
     }
 private:
-    io_service_ptr                              io_svc_;
-    ::asio_ns::steady_timer                     suspend_timer_;
-
-    ::std::shared_ptr<impl>                     pimpl_;
+    ::std::shared_ptr<runner>                   pimpl_;
 };
 
-io_service::id shared_work::service::id;
+using runner_ptr = ::std::shared_ptr<runner>;
+
+io_service::id runner::service::id;
+
+runner_ptr
+use_shared_work_algorithm(io_service_ptr io_svc)
+{
+    auto r = ::std::make_shared< runner >(io_svc);
+    ::boost::fibers::use_scheduling_algorithm< shared_work >( r );
+    return r;
+}
 
 } /* namespace fiber */
 } /* namespace asio */
